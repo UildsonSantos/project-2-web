09534ab2b9382b744c1ac2c5c2b9d5d3819242dc
UildsonSantos <wilder_shon@hotmail.com>
2019-06-14 15:16:24 -0300

    2 initial commit

--- handlers/multer.js
+++ handlers/multer.js
@@ -3,7 +3,7 @@ const multer = require('multer')
 module.exports = multer({
   storage:  multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    if(!file.mimetype.match(/mp4|jpe|jpeg|png|gif$i/)){
+    if(!file.mimetype.match(/|jpe|jpeg|png|gif$i/)){
       cb(new Error('File is not supported'), false)
       return
     }
